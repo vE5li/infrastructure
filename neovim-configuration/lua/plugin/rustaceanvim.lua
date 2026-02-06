d26dd727d6aedb171ea4c372b4356eaf19a060c5
vE5li <vE5li@tuta.io>
2026-02-06 14:21:01 +0100

    Fix overlapping Neovim bindings

--- neovim-configuration/lua/plugin/rustaceanvim.lua
+++ neovim-configuration/lua/plugin/rustaceanvim.lua
@@ -17,10 +17,10 @@ local function config()
                 vim.keymap.set('n', 'gE', function() vim.cmd.RustLsp('openDocs') end, desc "Go to external documentation")
                 vim.keymap.set('n', 'gC', function() vim.cmd.RustLsp('openCargo') end, desc "Go to Cargo.toml")
 
-                vim.keymap.set('n', 'gvh', function() vim.cmd.RustLsp({ 'view', 'hir' }) end, desc "View HIR")
-                vim.keymap.set('n', 'gvm', function() vim.cmd.RustLsp({ 'view', 'mir' }) end, desc "View MIR")
-                vim.keymap.set('n', 'gvH', function() vim.cmd.Rustc({ 'unpretty', 'hir' }) end, desc "Unpretty HIR")
-                vim.keymap.set('n', 'gvM', function() vim.cmd.Rustc({ 'unpretty', 'mir' }) end, desc "Unpretty MIR")
+                vim.keymap.set('n', 'gVh', function() vim.cmd.RustLsp({ 'view', 'hir' }) end, desc "View HIR")
+                vim.keymap.set('n', 'gVm', function() vim.cmd.RustLsp({ 'view', 'mir' }) end, desc "View MIR")
+                vim.keymap.set('n', 'gVH', function() vim.cmd.Rustc({ 'unpretty', 'hir' }) end, desc "Unpretty HIR")
+                vim.keymap.set('n', 'gVM', function() vim.cmd.Rustc({ 'unpretty', 'mir' }) end, desc "Unpretty MIR")
 
                 vim.keymap.set('n', 'gsr', ":RustLsp ssr ",
                     { noremap = true, buffer = arguments.buf, desc = "Structural search replace" })
